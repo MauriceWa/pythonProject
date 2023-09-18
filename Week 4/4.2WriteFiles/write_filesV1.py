d865a8103951da367b7ebdc56d4e3dba0c0d1c0c
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2023-09-18 19:57:26 +0200

    Finished 4.1/4.3

--- Week 4/4.2WriteFiles/write_filesV1.py
+++ Week 4/4.2WriteFiles/write_filesV1.py
@@ -1,6 +1,2 @@
 with open("Logs.txt", "a") as file:
-    input("")
-#input needs to add but not remove earlier entries, same issue on first entry, how to fix?
-
-# with open("Logs.txt", "w") as file:
-#     file.write("This is the first log entry")
+    file.write('\n' + input())
